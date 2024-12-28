dfeefc04144eb6c14fb6c2c00b10040946a12725
Damiano Scalise <damiano.scalise@gmail.com>
2024-12-28 23:28:01 +0100

    add shell.nix build environment

---
+++ shell.nix
@@ -0,0 +1,6 @@
+{pkgs ? import <nixpkgs> {}, ...}: {
+  default = pkgs.mkShell {
+    NIX_CONFIG = "extra-experimental-features = nix-command flakes ca-derivations";
+    nativeBuildInputs = with pkgs; [ autoconf269 automake115x libtool glib fuse libdbi libextractor ];
+  };
+}
